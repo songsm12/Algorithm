8bf0e2befc036b9c47dd3ab992cdb79591a08da2
shyunju7 <shyunju7@naver.com>
2022-05-24 20:20:58 +0900

    Add project

---
+++ pythonProject/baekjoon/baekjoon1000.py
@@ -0,0 +1,6 @@
+# 1000 A+B
+# 브론즈5
+# 수학 구현 사칙연산
+
+a, b = map(int, input().split())
+print(a+b, end="")
---
+++ pythonProject/main.py
@@ -0,0 +1,12 @@
+# Ctrl + shift +F10 -> 파일 새로 생성, 실행
+# Ctrl + F10 -> 현재 파일 실행
+
+# int 는 숫자타입
+# str 스트링 숫자를 문자로 변형
+
+# input()
+num = int(input())
+print(num)
+
+a, b = map(int, input().split())
+print(a, b)
